dd63bcb67dbab4fa3984d9c157006ff58572e018
Alexander Overvoorde <overv161@gmail.com>
2016-01-13 16:57:42 +0100

    Add heatmap

---
+++ genheatmap.py
@@ -0,0 +1,41 @@
+from __future__ import division
+import json
+
+data = []
+
+# Load data
+with open('html/data.csv') as f:
+    lines = f.readlines()
+    lines = map(lambda s: s.strip(), lines)
+    lines = map(lambda s: s.split(','), lines)
+
+    headers = lines[0]
+
+    lines = map(lambda lst: map(lambda v: v if ' ' in v else float(v), lst), lines[1:])
+
+    for row in lines:
+        data.append(dict(zip(headers, row)))
+
+# Discretize
+step_size = 5
+x_steps = int(105 / step_size + 0.5)
+y_steps = int(68 / step_size + 0.5)
+
+grid = [[0 for y in range(y_steps)] for x in range(x_steps)]
+
+for datum in data:
+    x = int(datum['x_pos'] / step_size)
+    y = int(datum['y_pos'] / step_size)
+
+    if x >= 0 and y >= 0 and x < x_steps and y < y_steps:
+        grid[x][y] += 1
+
+# Normalize
+max_value = max(map(max, grid))
+grid = map(lambda r: map(lambda v: v / max_value, r), grid)
+
+# Export
+with open('html/heatmap.json', 'w') as f:
+    json.dump(grid, f)
+
+print(x_steps, y_steps)
